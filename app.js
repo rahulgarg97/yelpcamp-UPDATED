90d9d37c180a5714197e344941b8b359158ba2f8
Rahul <rahulgarg0697@gmail.com>
2020-09-14 19:54:05 -0400

    modify listening port to deploy on heroku

--- app.js
+++ app.js
@@ -58,6 +58,7 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 
-app.listen(process.env.PORT, process.env.IP, function(){
-   console.log("The YelpCamp Server Has Started!");
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("The YelpCamp Server Has Started!");
 });
